45b975fccfedbc4a11dac64e69ca442af326778c
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-10-15 11:20:21 -0300

    Requisito 10, criando input que filtra as cartas

--- src/components/DeckOfCards.js
+++ src/components/DeckOfCards.js
@@ -3,6 +3,21 @@ import PropTypes from 'prop-types';
 import Card from './Card';
 
 class DeckOfCards extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      filterInputTxt: '',
+    };
+
+    this.onInputFilterChange = this.onInputFilterChange.bind(this);
+  }
+
+  onInputFilterChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
+  }
+
   creatCardUsingObj(cardObj, buttonFunction) {
     const { cardName,
     } = cardObj;
@@ -19,13 +34,28 @@ class DeckOfCards extends React.Component {
 
   render() {
     const { deck, onDeleteButtonClick } = this.props;
+    const { filterInputTxt } = this.state;
     return (
       <section>
+        <div>
+          <h4>Filtro de busca</h4>
+          <input
+            data-testid="name-filter"
+            name="filterInputTxt"
+            type="text"
+            onChange={ this.onInputFilterChange }
+          />
+        </div>
         <h1>Todas as cartas</h1>
         {
-          deck.map((cardObj, index) => this.creatCardUsingObj(cardObj, () => {
-            onDeleteButtonClick(index);
-          }))
+          deck.map((cardObj, index) => {
+            if (cardObj.cardName.includes(filterInputTxt)) {
+              return (this.creatCardUsingObj(cardObj, () => {
+                onDeleteButtonClick(index);
+              }));
+            }
+            return '';
+          })
         }
       </section>
     );
